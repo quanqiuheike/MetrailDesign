4b4699d896dfebcfb6f2eff77bd6f853a945a3ec
quanqiuheike <1831814662@qq.com>
2017-04-17 18:11:17 +0800

    添加的功能1，打印log

--- MaterialDesign/src/main/java/com/cc/md/business/App.java
+++ MaterialDesign/src/main/java/com/cc/md/business/App.java
@@ -1,15 +1,18 @@
 package com.cc.md.business;
 
 import android.app.Application;
+import android.util.Log;
+
 
 /**
  * Created by cheng.qx on 2016/12/2.
  */
 
 public class App extends Application {
+    private final String TAG=getClass().getSimpleName();
     @Override
     public void onCreate() {
         super.onCreate();
-        
+        Log.i(TAG, "用于测试的功能1");
     }
 }
